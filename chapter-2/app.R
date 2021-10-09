f61649d08a0e93f1f5dee330fec719262a6c7f36
Cory Lanker <1787180+clanker@users.noreply.github.com>
2021-10-09 14:22:22 -0700

    Second exercises for Chapter 2

--- chapter-2/app.R
+++ chapter-2/app.R
@@ -10,7 +10,8 @@
 library(shiny)
 
 # k = 1.1-1.4:  Exercises pp. 21-22
-k <- 1.4
+# k = 2.1-2.4:  Exercises p. 26
+k <- 2.4
 
 if (k == 1.1) {  ## Exercise 1, p. 21
     ui <- fluidPage(
@@ -86,5 +87,68 @@ if (k == 1.4) {  ## Exercise 4, p. 22
     }
 }
 
+if (k == 2.1) {  ## Exercise 1, p. 26
+    ui <- fluidPage(
+        verbatimTextOutput("part_1a"),
+        textOutput("part_1b"),
+        "Part 1c.",
+#        textOutput("part_1c"),
+        verbatimTextOutput("part_1c"),
+        "Part 1d.",
+        textOutput("part_1d"),
+#        verbatimTextOutput("part_1d"),
+#        "Part 1d without str.",
+#        textOutput("part_1e"),
+#        verbatimTextOutput("part_1e"),
+        "Part 1d with renderPrint and without str.",
+#        textOutput("part_1f"),
+        verbatimTextOutput("part_1f"),
+    )
+    
+    server <- function(input, output, session) {
+        output$part_1a <- renderPrint(summary(mtcars))
+        output$part_1b <- renderText("Good morning!")
+        output$part_1c <- renderPrint(t.test(1:5, 2:6))
+        output$part_1d <- renderText(str(lm(mpg ~ wt, data = mtcars)))
+        #output$part_1e <- renderText(lm(mpg ~ wt, data = mtcars))
+        output$part_1f <- renderPrint(lm(mpg ~ wt, data = mtcars))
+    }
+}
+
+if (k == 2.2) {  ## Exercise 2, p. 26
+    ui <- fluidPage(
+        plotOutput("plot", width = "700px", height = "300px")
+    )
+    server <- function(input, output, session) {
+        output$plot <- renderPlot(plot(1:5))
+    }
+}
+
+if (k == 2.3) {  ## Exercise 3, p. 26
+    ui <- fluidPage(
+        dataTableOutput("table")
+    )
+    server <- function(input, output, session) {
+        output$table <- renderDataTable(
+            mtcars, options = list(pageLength = 5, ordering = F, searching = F)
+            )
+    }
+}
+
+if (k == 2.4) {  ## Exercise 4, p. 26
+    library(reactable)
+    
+    ui <- fluidPage(
+        reactableOutput("table")
+    )
+    server <- function(input, output, session) {
+        output$table <- renderReactable({
+            reactable(mtcars,
+                      filterable = TRUE,
+                      searchable = TRUE,
+                      minRows = 10)
+        })
+    }
+}
 # Run the application 
 shinyApp(ui, server)
