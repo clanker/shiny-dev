97acd6218a95fddab72d6ec2c7a6a39859ae1b06
Cory Lanker <1787180+clanker@users.noreply.github.com>
2021-10-12 09:01:41 -0700

    Start of Chapter 4: EDA

---
+++ chapter-4/app.R
@@ -0,0 +1,16 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(vroom)
+library(tidyverse)
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
